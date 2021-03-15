1aa22a78d291dac8fc44adc11968807de52afd3d
Milan M2 <mmeldo@gmail.com>
2021-03-15 17:32:59 +0100

    Update import scripts (#3)

--- scripts/import-cgdb.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const CardgameDbToThronetekiConverter = require('../src/CardgameDbToThronetekiConverter');
-
-let converter = new CardgameDbToThronetekiConverter();
-
-converter.convert({
-    pathToPackFile: path.join(process.cwd(), process.argv[2]),
-    cyclePrefix: process.argv[3]
-});
